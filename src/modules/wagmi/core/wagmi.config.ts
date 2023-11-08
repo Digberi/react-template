f7926d263db199750872f97e9665240948ed8e82
Digberi <orkepuma@gmail.com>
2023-11-08 19:09:07 +0200

    add web3 auth

--- src/modules/wagmi/core/wagmi.config.ts
+++ src/modules/wagmi/core/wagmi.config.ts
@@ -1,14 +1,17 @@
-import { defaultChains } from '@modules/wagmi/config';
-import { InjectedConnector, configureChains, createConfig } from '@wagmi/core';
+import { Web3AuthConnectorInstance } from '@modules/web3-auth/get-web3-auth-connector';
+import { configureChains, createConfig } from '@wagmi/core';
 import { publicProvider } from 'wagmi/providers/public';
+
+import { defaultChains } from '../config';
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(defaultChains, [
   publicProvider()
 ]);
 
-export const Connector = new InjectedConnector({
-  chains
-});
+// export const Connector = new InjectedConnector({
+//   chains
+// });
+export const Connector = Web3AuthConnectorInstance(chains);
 
 export const wagmiConfig = createConfig({
   autoConnect: true,
